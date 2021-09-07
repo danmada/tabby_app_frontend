8de514ed1407eab6f42a83968ff1571168d84a05
danmada <danmada@gmail.com>
2021-09-07 09:24:21 -0500

    add adding drinks to tab

--- src/components/TabDisplay.js
+++ src/components/TabDisplay.js
@@ -5,8 +5,8 @@ import DrinksList from "./DrinksList";
 
 function TabDisplay() {
 const [indTab, setIndTab] = useState([])
-const [barId, setBarId] = useState([])
 const [drinksList, setDrinksList] = useState([])
+const [barId, setBarId] = useState()
 const params = useParams()
 
 
@@ -16,25 +16,41 @@ useEffect(() => {
     fetch(`http://localhost:3000/tabs/${params.id}`)
     .then((res) => res.json())
     .then((json) => {
+        setBarId(json.bar_id)
         const drinks = json.orders
         setIndTab(drinks)
+
     })
     .catch((err) => {
         console.log(err);
-      })
+    })
 }, [params.id]);
 
-console.log(indTab)
 
-// function handleDisplayDrinks() {
-//     fetch(`http://localhost:3000/drinks`)
-//     .then((res) => res.json())
-//     .then((json) => {
-//         setDrinksList(json)
-//     })
 
-// }
+function handleDisplayDrinks() {
+    fetch(`http://localhost:3000/drinks`)
+    .then((res) => res.json())
+    .then((json) => {
+        setDrinksList(json)
+    })
 
+}
+
+function handleAddDrink(e) {
+    console.log(params.id)
+    console.log(e.target.value)
+    fetch("http://localhost:3000/orders", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+        tab_id: params.id,
+        drink_id: e.target.value,
+    }),
+    });
+}
 
 
 
@@ -43,17 +59,22 @@ console.log(indTab)
     return (
         <div>
         <h1>Tab Display</h1>
-        {/* <button onClick={handleDisplayDrinks}>Add New Item</button> */}
         <div>
-        {indTab.map((order) => <h4>{order.drink.drink_type} - ${order.drink.price}</h4>)}
+        {indTab.map((order) => 
+            <h4>{order.drink.drink_type} - ${order.drink.price}</h4>
+        )}
+        <button onClick={handleDisplayDrinks}>Add New Item</button>
         </div>
-        {/* {drinksList.map((drink) => {
-            if (drink.bar_id === barId) {
-                <p>{drink.name}</p>
-            } else {
-                return null
-            }
-        })} */}
+        {drinksList.map((drink) => {
+            if (drink.bar_id == barId) {
+                return  <div>
+                            <h4>{drink.drink_type}</h4>
+                            <p>{drink.id}</p>
+                            <button value={drink.id} onClick={handleAddDrink}>Add</button>
+                        </div>
+            } 
+        })}
+
         </div>
     )
 
